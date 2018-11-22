30219e6b82b830da2597a6b1b060511880675df8
Minakshi-Chidrawar <minakshichidrawar@gmail.com>
2018-11-22 20:31:46 +0000

    js with querySelectorAll

---
+++ js/querySelectorAll.js
@@ -0,0 +1,10 @@
+function showalert(){
+	alert('You Clicked Me');
+}
+
+function clickme() {
+	var list = document.querySelectorAll(#main p");
+	list[0].onclick=showalert;
+}
+
+window.onload=clickme;
