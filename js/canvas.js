5871be5c843c0842e446bdf00b5782159a30980d
Minakshi-Chidrawar <minakshichidrawar@gmail.com>
2018-12-08 23:07:54 +0000

    Use of linearGradient

--- js/canvas.js
+++ js/canvas.js
@@ -3,12 +3,21 @@ function initiate()
 	var elem = document.getElementById('canvas');
 	canvas = elem.getContext('2d');
 	
+	/* 
 	canvas.fillStyle = '#000099';
 	canvas.strokeStyle = '#990000';
 	
 	canvas.strokeRect(100, 100, 120, 120);
 	canvas.fillRect(110, 110, 100, 100);
 	canvas.clearRect(120, 120, 80, 80);
+	*/
+	var grad = canvas.createLinearGradient(0, 0, 10, 100);
+	grad.addColorStop(0.5, '#0000FF');
+	grad.addColorStop(1, '#000000');
+	canvas.fillStyle = grad;
+	
+	canvas.fillRect(10, 10, 100, 100);
+	canvas.fillRect(150, 10, 200, 100);
 }
 
 window.addEventListener('load', initiate, false);
