67f143f388ed3584ddd6e7785a77cba0ab26f55e
Minakshi-Chidrawar <minakshichidrawar@gmail.com>
2018-12-09 19:42:12 +0000

    Use canvas.shadowColor

--- js/canvas.js
+++ js/canvas.js
@@ -71,14 +71,23 @@ function initiate()
 	canvas.lineTo(215, 155);
 	canvas.lineTo(195, 155);
 	canvas.stroke();
-	*/
 	
+	// use of measureText
 	canvas.font = 'bold 24px verdana, sans-serif';
 	canvas.textAlign = 'start';
 	canvas.textBaseline = 'bottom';
 	canvas.fillText('My message', 100, 124);
 	var size = canvas.measureText('My message');
 	canvas.strokeRect(100, 100, size.width+50, 24);
+	*/
+	
+	canvas.shadowColor = 'rgba(0, 0, 0, 0.5)';
+	canvas.shadowOffsetX = 4;
+	canvas.shadowOffsetY = 4;
+	canvas.shadowBlur = 5;
+	
+	canvas.font = 'bold 50px verdana, sans-serif';
+	canvas.fillText('My message', 100, 100);
 }
 
 window.addEventListener('load', initiate, false);
